c648423c11d5b4894b5bf488b9e5bdda77e59e03
Nikhil Gunti <nikhilgunti73@gmail.com>
2021-10-03 12:05:06 -0500

    Update inference.py
    Updated string formatting part for class label and blurriness score

--- inference.py
+++ inference.py
@@ -1,11 +1,13 @@
 import joblib
 import cv2
 import os
+
+from matplotlib.pyplot import cla
 from blur_functions import varMaxLaplacian, varMaxSobel
 import argparse
 
 def main():
-    #Loading saved Random Forest model
+    #Loading saved pre-trained Random Forest model
     model = joblib.load('model.joblib')
 
     # Adding argument to input image path
@@ -13,7 +15,7 @@ def main():
     parser.add_argument('--image', type=str,required=True)
     args = parser.parse_args()
     img_path = os.path.join(os.getcwd(), "Images", args.image)
-    print(img_path)
+    
     #Reading input image and converting it to gray scale
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,10 +24,15 @@ def main():
     lapvar, lapmax = varMaxLaplacian(gray)
     sobvar, sobmax = varMaxSobel(gray)
 
+    #Label and score prediction based on pre-trained random forest model
     class_label = ["Clear" if model.predict([[lapmax, lapvar, sobmax, sobvar]])==0 else "Blur"]
     blur_score = model.predict_proba([[lapmax, lapvar, sobmax, sobvar]])
 
-    print(f"The image is {class_label} with a bluriness score of {blur_score[0][1]}")
+    # Label and Score formatting
+    class_label = ''.join(class_label)
+    blur_score = str(round(blur_score[0][1],2))
+
+    print(f"The image is {class_label} with a bluriness score of {blur_score}")
 
 
 if __name__=='__main__':
