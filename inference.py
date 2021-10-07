3fa145cb0bd02c27d868b288d6048d98a487e777
Nikhil Gunti <nikhilgunti73@gmail.com>
2021-10-06 21:34:59 -0500

    Streamlit web app

--- inference.py
+++ inference.py
@@ -16,23 +16,25 @@ def main():
     args = parser.parse_args()
     img_path = os.path.join(os.getcwd(), "Images", args.image)
     
-    #Reading input image and converting it to gray scale
-    img = cv2.imread(img_path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    while os.path.isfile(img_path) is True:
+        #Reading input image and converting it to gray scale
+        img = cv2.imread(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #Extracting Laplacian filter and Sobel filter values
-    lapvar, lapmax = varMaxLaplacian(gray)
-    sobvar, sobmax = varMaxSobel(gray)
+        #Extracting Laplacian filter and Sobel filter values
+        lapvar, lapmax = varMaxLaplacian(gray)
+        sobvar, sobmax = varMaxSobel(gray)
 
-    #Label and score prediction based on pre-trained random forest model
-    class_label = ["Clear" if model.predict([[lapmax, lapvar, sobmax, sobvar]])==0 else "Blur"]
-    blur_score = model.predict_proba([[lapmax, lapvar, sobmax, sobvar]])
+        #Label and score prediction based on pre-trained random forest model
+        class_label = ["Blur" if model.predict([[lapmax, lapvar, sobmax, sobvar]])==1 else "Clear"]
+        blur_score = model.predict_proba([[lapmax, lapvar, sobmax, sobvar]])
 
-    # Label and Score formatting
-    class_label = ''.join(class_label)
-    blur_score = str(round(blur_score[0][1],2))
+        # Label and Score formatting
+        class_label = ''.join(class_label)
+        blur_score = str(round(blur_score[0][1],2))
 
-    print(f"The image is {class_label} with a bluriness score of {blur_score}")
+        #print(f"The image has a clarity score of {blur_score}")
+        return class_label, blur_score
 
 
 if __name__=='__main__':
---
+++ streamlit.py
@@ -0,0 +1,61 @@
+from glob import glob
+from cv2 import data
+from numpy.random import choice
+import streamlit as st
+import joblib
+import cv2
+import os
+import glob
+from matplotlib.pyplot import cla
+import pandas as pd
+import numpy as np
+from blur_functions import varMaxLaplacian, varMaxSobel
+import argparse
+from PIL import Image
+
+
+st.title("Image Blur Detection")
+
+st.markdown("### 🎲 The Application")
+st.markdown("This application is to detect whether an input image fed is blur or not with a blurriness score")
+
+
+menu = ["Select image from the below list", "Upload From Computer"]
+choice = st.sidebar.selectbox("Menu", menu)
+
+if choice == "Select image from the below list":
+    uploaded_file = st.sidebar.selectbox("choose your image", glob.glob(os.path.join(os.getcwd(), "Images", "*")))
+else:
+    uploaded_file = st.sidebar.file_uploader("Please upload an image:", type=['jpeg','jpg', 'png'])
+
+#Loading model
+model = joblib.load("model.joblib")
+
+
+def predict(img_file):
+    img = Image.open(img_file)
+    st.image(img, caption="Uploaded image", use_column_width=True)
+    new_img = np.asarray(img.convert('RGB'))
+    gray = cv2.cvtColor(new_img,1)
+
+    #Extracting Laplacian filter and Sobel filter values
+    lapvar, lapmax = varMaxLaplacian(gray)
+    sobvar, sobmax = varMaxSobel(gray)
+
+    columns=["Laplacian maximum", "Laplacian variance", "Sobel maximum", "Sobel Variance"]
+    features = [lapmax, lapvar, sobmax, sobvar]
+    res_dict = dict(zip(columns, features))
+    #Label and score prediction based on pre-trained random forest model
+    class_label = ["Blur" if model.predict([features])==1 else "Clear"]
+    blur_score = model.predict_proba([features])
+
+    # Label and Score formatting
+    class_label = ''.join(class_label)
+    blur_score = round(blur_score[0][1],2)
+    return class_label, blur_score, res_dict
+
+if uploaded_file is not None:
+    class_label, blur_score, res_dict = predict(uploaded_file)
+    st.write('Image is : %s (%.2f)' % (class_label, blur_score))
+    expander = st.expander("For more details !!")
+    expander.write(res_dict)
